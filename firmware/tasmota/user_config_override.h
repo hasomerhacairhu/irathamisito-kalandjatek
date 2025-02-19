12733c428ce7786708917588eb0ad29542123f2b
bedomarci <bedomarci@gmail.com>
2025-02-19 17:47:36 +0100

    adding firmware, update folder structure

---
+++ firmware/tasmota/user_config_override.h
@@ -0,0 +1,63 @@
+/*
+  user_config_override.h - user configuration overrides my_user_config.h for Tasmota
+
+  Copyright (C) 2021  Theo Arends
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _USER_CONFIG_OVERRIDE_H_
+#define _USER_CONFIG_OVERRIDE_H_
+
+/*****************************************************************************************************\
+ * USAGE:
+ *   To modify the stock configuration without changing the my_user_config.h file:
+ *   (1) copy this file to "user_config_override.h" (It will be ignored by Git)
+ *   (2) define your own settings below
+ *
+ ******************************************************************************************************
+ * ATTENTION:
+ *   - Changes to SECTION1 PARAMETER defines will only override flash settings if you change define CFG_HOLDER.
+ *   - Expect compiler warnings when no ifdef/undef/endif sequence is used.
+ *   - You still need to update my_user_config.h for major define USE_MQTT_TLS.
+ *   - All parameters can be persistent changed online using commands via MQTT, WebConsole or Serial.
+\*****************************************************************************************************/
+
+#undef  STA_SSID1
+#define STA_SSID1         "irathamisito-network"             // [Ssid1] Wifi SSID
+
+#undef  STA_PASS1
+#define STA_PASS1         "Rootpass"     // [Password1] Wifi password
+
+#undef  MQTT_HOST
+#define MQTT_HOST         "192.168.13.90" // [MqttHost]
+
+#undef  MQTT_PORT
+#define MQTT_PORT         1883                   // [MqttPort] MQTT port (10123 on CloudMQTT)
+
+#undef  MQTT_USER
+#define MQTT_USER         "irathamisito"         // [MqttUser] Optional user
+
+#undef  MQTT_PASS
+#define MQTT_PASS         "Rootpass"         // [MqttPassword] Optional password
+
+#define USE_I2S_ALL
+#define USE_A4988_STEPPER 
+
+/*
+#define ROTARY_V1 ez lehet kell később not sure
+*/
+
+
+#endif  // _USER_CONFIG_OVERRIDE_H_
--- tasmota/autoexec/radio1.be
+++ tasmota/autoexec/radio1.be
@@ -2,5 +2,6 @@ tasmota.cmd("I2SGain 30")
 tasmota.cmd("SetOption114 1")
 tasmota.cmd("SwitchMode0 15")
 tasmota.cmd("I2SGain 30")
+tasmota.cmd("Template {\"NAME\":\"Radio1\",\"GPIO\":[6720,672,704,736,7840,7776,0,0,7808,160,161,162,0,0,163,224,1,225,226,227,0,228,229,230,0,0,0,0,231,232,164,165,166,0,0,167],\"FLAG\":0,\"BASE\":1}")
 
 #sd speaker gombok ledek
